7c46807ee7f88009266851e43a4006dcb055372e
TrevNowak <trevelyannowakowski@gmail.com>
2025-03-14 12:16:35 +0800

    NBR

--- src/Negative_Binomial_Regression.py
+++ src/Negative_Binomial_Regression.py
@@ -6,23 +6,20 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import statsmodels.api as sm
 
-# Load Data
 df = pd.read_csv("data/simulated_dengue_data.csv")
 
-# Define Independent and Dependent Variables
 X = df[[
     "Rainfall_mm_lag1", "Rainfall_mm_lag2", "Rainfall_mm_lag3",
     "Temperature_C", "Humidity_%", "Fogging", "Larvicide_Use", "Seasonality",
     "Population_Density", "Rainfall_Temperature_Interaction",
     "Fogging_Cases_Interaction", "Larvicide_Cases_Interaction"
 ]]
-X = sm.add_constant(X)  # Add intercept
+X = sm.add_constant(X) 
 y = df["Simulated_Dengue_Cases_Noised"]
 
-# Drop NaN values caused by lagging
+#Drop NaN values caused by lagging
 df_cleaned = df.dropna()
 
-# Re-define X and y after dropping NaN rows
 X = df_cleaned[[ 
     "Rainfall_mm_lag1", "Rainfall_mm_lag2", "Rainfall_mm_lag3",
     "Temperature_C", "Humidity_%", "Fogging", "Larvicide_Use", "Seasonality",
@@ -30,12 +27,10 @@ X = df_cleaned[[
     "Fogging_Cases_Interaction", "Larvicide_Cases_Interaction"
 ]]
 
-X = sm.add_constant(X)  # Add intercept
+X = sm.add_constant(X)
 y = df_cleaned["Simulated_Dengue_Cases_Noised"] 
 
-# Fit Negative Binomial Model
 model = sm.GLM(y, X, family=sm.families.NegativeBinomial())
 result = model.fit()
 
-# Print Summary
 print(result.summary())
